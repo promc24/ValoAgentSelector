154fb968efcb1c0872aa595ce9e6947964163ad8
promc24 <promodic24@yahoo.it>
2023-11-07 02:29:34 +0100

    ability details and role details fetchable

--- main/database_connect.py
+++ main/database_connect.py
@@ -1,55 +1,51 @@
 import sqlite3
 import os
 
-db_path = 'database/agents_details.db'
+db_path = 'database/valorant_agents.db'
+agents_outcome = []
 
 def database_connection(agent_number):
 
+    #excutes sql and fetches the needed data
     def access_db():
+            
+            try:
+                #Connects to DB and creates a cursor
+                sqliteConnection = sqlite3.connect(db_path)
+                cursor = sqliteConnection.cursor()
+                # Query eecuted with cursor
+                cursor.execute("SELECT * FROM agents_details ORDER BY RANDOM() LIMIT " + str(agent_number) +";")
+                #Fetchs and outputs results
+                result = cursor.fetchall()
+                for row in result:
+                    agents_outcome.append(row)
+                #Closes DB connecton
+                sqliteConnection.close()
+            #Handle errors
+            except sqlite3.Error as error:
+                print('Error occurred - ', error)
+                 
+    
 
-            sqliteConnection = sqlite3.connect(db_path)
-            cursor = sqliteConnection.cursor()
-
-            cursor.execute("SELECT * FROM agents_details ORDER BY RANDOM() LIMIT " + str(agent_number) +";")
-            # Fetch and output result
-            result = cursor.fetchall()
-            for row in result:
-                print(row)
-
-            sqliteConnection.close()
-
+    #Checks if the database exists
     if os.path.exists(db_path):
         access_db()
 
     else:
-
-
-
         sqliteConnection = None
             
         try:
         
-            # Connect to DB and create a cursor
-            sqliteConnection = sqlite3.connect(db_path)
-            
-            #print('DB created')
-        
-            # Write a query and execute it with cursor
-            sql_file = open('database/agents_details.sql', 'r')
+            #Connects to DB and creates a cursor
+            sqliteConnection = sqlite3.connect(db_path)       
+            #Writes a query and executes it with cursor
+            sql_file = open('database/valorant_agents.sql', 'r')
             query = sql_file.read()
 
             cursor = sqliteConnection.cursor()
             print('connection SQLIte established')
             cursor.executescript(query)
-            #sqliteConnection.commit()
-            #print(query)
-        
-            # Fetch and output result
-            #result = cursor.fetchall()
-            #print(result)
-            
-        
-            # Close the cursor
+            #Closes DB connection and runs the data fetching function
             sqliteConnection.close()
             access_db()
         
@@ -57,10 +53,71 @@ def database_connection(agent_number):
         except sqlite3.Error as error:
             print('Error occurred - ', error)
         
-        # Close DB Connection irrespective of success
-        # or failure
-        #finally:
+def get_role_details(role, agent_num):
+
+    try:
+        #Connects to DB and creates a cursor
+        sqliteConnection = sqlite3.connect(db_path)
+        cursor = sqliteConnection.cursor()
+        # Query eecuted with cursor
+        cursor.execute("SELECT role_details FROM agents_role WHERE a_role = '" + role + "'")
+        #Fetchs and outputs results
+        result = cursor.fetchall()
+        print(role + '\n\n' + result[0][0])
+        #Closes DB connecton
+        sqliteConnection.close()
+
+        ability_info = input("Would you like to learn about the Agent's abilities? ").upper()
+        if ability_info == 'Y' or ability_info == 'YES':
+            get_ability_details(agent_num)
+        else:
+            agents_outcome.clear()
+            print('Thank you for using this program!')
+
+    #Handle errors
+    except sqlite3.Error as error:
+        print('Error occurred - ', error)
+
+def get_ability_details(agent_num, role):
+
+    try:
+        #Connects to DB and creates a cursor
+        sqliteConnection = sqlite3.connect(db_path)
+        cursor = sqliteConnection.cursor()
+        # Query eecuted with cursor
+        cursor.execute("SELECT ability_name FROM agents_abilities WHERE a_num = '" + str(agent_num) + "'")
+        #Fetchs and outputs results
+        result = cursor.fetchall()
+        #print(role + '\n\n')
+        print('Special Abilities names:\nQ. ' + result[0][0] + '\nE. ' + result[1][0] + '\nC. ' + result[2][0] + '\nX. ' + result[3][0])
+        #Closes DB connecton
         
-            #if sqliteConnection:
-                #sqliteConnection.close()
-                #print('SQLite Connection closed')
+        ability_detail = input('Type the ability letter to see its details or 1 to see all the details together: ').upper()
+        if ability_detail == 'Q':
+            cursor.execute("SELECT ability_details FROM agents_abilities WHERE ability_name = '" + result[0][0] + "'")
+            outcome = cursor.fetchall()
+            print(result[0][0] + '\n\n' + outcome[0][0] )
+        elif ability_detail == 'E':
+            cursor.execute("SELECT ability_details FROM agents_abilities WHERE ability_name = '" + result[1][0] + "'")
+            outcome = cursor.fetchall()
+            print(result[1][0] + '\n\n' + outcome[0][0] )
+        elif ability_detail == 'C':
+            cursor.execute("SELECT ability_details FROM agents_abilities WHERE ability_name = '" + result[2][0] + "'")
+            outcome = cursor.fetchall()
+            print(result[2][0] + '\n\n' + outcome[0][0] )
+        elif ability_detail == 'X':
+            cursor.execute("SELECT ability_details FROM agents_abilities WHERE ability_name = '" + result[3][0] + "'")
+            outcome = cursor.fetchall()
+            print(result[3][0] + '\n\n' + outcome[0][0] )
+        
+        
+        sqliteConnection.close()
+        ability_info = input("Would you like to learn about the Agent's role? ").upper()
+        if ability_info == 'Y' or ability_info == 'YES':
+            get_role_details(role, agent_num)
+        else:
+            agents_outcome.clear()
+            print('Thank you for using this program!')
+    #Handle errors
+    except sqlite3.Error as error:
+        print('Error occurred - ', error)
--- main/valorant.py
+++ main/valorant.py
@@ -8,6 +8,23 @@ import database_connect
 
     
     #return print(random.choices(list(list_of_agents), k=agent_num))
+#Get further information of agents dependin gon user's needs
+def get_agents_info():
+    info = input('Would you like some info of the agents?').upper()
+    #Check if user requires more info
+    if info == 'Y' or info == 'YES':
+        specification = int(input('Please specify which agent by typing the agent number: '))
+        for agent in database_connect.agents_outcome:
+            if specification == agent[0]:
+                wanted_info = int(input('Select one of the option by typing the corrisponding number:\n 1. Role Description\n 2. Agent Abilities\n'))
+                if wanted_info == 1:
+                    database_connect.get_role_details(agent[2], agent[0])
+                else:
+                    database_connect.get_ability_details(agent[0], agent[2])
+                    
+    else:
+        database_connect.agents_outcome.clear()
+        print('Thank you for using this program!')
 
 def user_input():
     num = input('How many agents do you need? ')
@@ -17,5 +34,13 @@ def user_input():
         return user_input()
     else:
         database_connect.database_connection(agent_number)
+        print('Angent n° | Name | Role')
+        for agent in database_connect.agents_outcome:
+            
+            print('-' + ' ' + str(agent[0]) + ' | ' + agent[1] + ' | ' + agent[2])
+        get_agents_info()
+
+
+
 
 user_input()
