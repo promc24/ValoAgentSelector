e6a76d2ba5a04eb84bf387bdc99df9fedc98daaf
promc24 <promodic24@yahoo.it>
2023-11-09 03:59:57 +0100

    loops used to maintain a smoother return to inputs

--- main/database_connect.py
+++ main/database_connect.py
@@ -1,21 +1,23 @@
 import sqlite3
 import os
 
+#global variables
 db_path = 'database/valorant_agents.db'
 agents_outcome = []
+end_program_msg = 'Thank you for using this program!'
+
 
 def database_connection(agent_number):
-
     #excutes sql and fetches the needed data
     def access_db():
             
             try:
-                #Connects to DB and creates a cursor
+                #Connect to DB and creates a cursor
                 sqliteConnection = sqlite3.connect(db_path)
                 cursor = sqliteConnection.cursor()
-                # Query eecuted with cursor
+                #Fetch the random agents numbers
                 cursor.execute("SELECT * FROM agents_details ORDER BY RANDOM() LIMIT " + str(agent_number) +";")
-                #Fetchs and outputs results
+                #Fetch and outputs results
                 result = cursor.fetchall()
                 for row in result:
                     agents_outcome.append(row)
@@ -43,7 +45,8 @@ def database_connection(agent_number):
             query = sql_file.read()
 
             cursor = sqliteConnection.cursor()
-            print('connection SQLIte established')
+            #print('connection SQLIte established')
+            #executes the database script and creates the DB
             cursor.executescript(query)
             #Closes DB connection and runs the data fetching function
             sqliteConnection.close()
@@ -52,72 +55,115 @@ def database_connection(agent_number):
         # Handle errors
         except sqlite3.Error as error:
             print('Error occurred - ', error)
-        
+
+#Function to fetch agent role details   
 def get_role_details(role, agent_num):
 
     try:
         #Connects to DB and creates a cursor
         sqliteConnection = sqlite3.connect(db_path)
         cursor = sqliteConnection.cursor()
-        # Query eecuted with cursor
+        #fetch role details thorugh the role name
         cursor.execute("SELECT role_details FROM agents_role WHERE a_role = '" + role + "'")
-        #Fetchs and outputs results
+        #Fetch and outputs results
         result = cursor.fetchall()
         print(role + '\n\n' + result[0][0])
         #Closes DB connecton
         sqliteConnection.close()
 
+        #Directs user to role and ability info menu if yes
         ability_info = input("Would you like to learn about the Agent's abilities? ").upper()
         if ability_info == 'Y' or ability_info == 'YES':
-            get_ability_details(agent_num)
+            get_ability_details(agent_num, role)
         else:
+            #Empty list and end program
             agents_outcome.clear()
-            print('Thank you for using this program!')
+            print(end_program_msg)
 
     #Handle errors
     except sqlite3.Error as error:
         print('Error occurred - ', error)
 
+#Function to fetch Ability informations
 def get_ability_details(agent_num, role):
 
     try:
         #Connects to DB and creates a cursor
         sqliteConnection = sqlite3.connect(db_path)
         cursor = sqliteConnection.cursor()
-        # Query eecuted with cursor
+        #Fetch anilities names thought agent number
         cursor.execute("SELECT ability_name FROM agents_abilities WHERE a_num = '" + str(agent_num) + "'")
         #Fetchs and outputs results
         result = cursor.fetchall()
         #print(role + '\n\n')
         print('Special Abilities names:\nQ. ' + result[0][0] + '\nE. ' + result[1][0] + '\nC. ' + result[2][0] + '\nX. ' + result[3][0])
-        #Closes DB connecton
+        #starts while loop
+        while True:
+            ability_detail = input('Type the ability letter to see its details or 1 to see all the details together: ').upper()
+            #depending on user answer fetches corrisponding info
+            if ability_detail == 'Q':
+                cursor.execute("SELECT ability_details FROM agents_abilities WHERE ability_name = '" + result[0][0] + "'")
+                outcome = cursor.fetchall()
+                print(result[0][0] + '\n\n' + outcome[0][0])
+                check_rest = input('Would you like to check the other abilities? ').upper()
+                #breaks or continues the loop depensing if user wants more info 
+                if check_rest == 'Y' or check_rest == 'YES':
+                    continue
+                else:
+                    break
+            elif ability_detail == 'E':
+                cursor.execute("SELECT ability_details FROM agents_abilities WHERE ability_name = '" + result[1][0] + "'")
+                outcome = cursor.fetchall()
+                print(result[1][0] + '\n\n' + outcome[0][0])
+                check_rest = input('Would you like to check the other abilities? ').upper()
+                #breaks or continues the loop depensing if user wants more info 
+                if check_rest == 'Y' or check_rest == 'YES':
+                    continue
+                else:
+                    break
+            elif ability_detail == 'C':
+                cursor.execute("SELECT ability_details FROM agents_abilities WHERE ability_name = '" + result[2][0] + "'")
+                outcome = cursor.fetchall()
+                print(result[2][0] + '\n\n' + outcome[0][0])
+                check_rest = input('Would you like to check the other abilities? ').upper()
+                #breaks or continues the loop depensing if user wants more info 
+                if check_rest == 'Y' or check_rest == 'YES':
+                    continue
+                else:
+                    break
+            elif ability_detail == 'X':
+                cursor.execute("SELECT ability_details FROM agents_abilities WHERE ability_name = '" + result[3][0] + "'")
+                outcome = cursor.fetchall()
+                print(result[3][0] + '\n\n' + outcome[0][0])
+                check_rest = input('Would you like to check the other abilities? ').upper()
+                #breaks or continues the loop depensing if user wants more info 
+                if check_rest == 'Y' or check_rest == 'YES':
+                    continue
+                else:
+                    break
+            elif ability_detail == '1':
+                #fetched all ability names and details and breaks the loop
+                cursor.execute("SELECT ability_name, ability_details FROM agents_abilities WHERE a_num = '" + str(agent_num) + "'")
+                outcome = cursor.fetchall()
+                print('- ' + outcome[0][0] + '\n\n' + outcome[0][1] + '\n\n')
+                print('- ' + outcome[1][0] + '\n\n' + outcome[1][1] + '\n\n')
+                print('- ' + outcome[2][0] + '\n\n' + outcome[2][1] + '\n\n')
+                print('- ' + outcome[3][0] + '\n\n' + outcome[3][1] + '\n\n')
+                break
+            else:
+                #Warn message and restarts loop
+                print('Please make sure you typed the right letter!')
         
-        ability_detail = input('Type the ability letter to see its details or 1 to see all the details together: ').upper()
-        if ability_detail == 'Q':
-            cursor.execute("SELECT ability_details FROM agents_abilities WHERE ability_name = '" + result[0][0] + "'")
-            outcome = cursor.fetchall()
-            print(result[0][0] + '\n\n' + outcome[0][0] )
-        elif ability_detail == 'E':
-            cursor.execute("SELECT ability_details FROM agents_abilities WHERE ability_name = '" + result[1][0] + "'")
-            outcome = cursor.fetchall()
-            print(result[1][0] + '\n\n' + outcome[0][0] )
-        elif ability_detail == 'C':
-            cursor.execute("SELECT ability_details FROM agents_abilities WHERE ability_name = '" + result[2][0] + "'")
-            outcome = cursor.fetchall()
-            print(result[2][0] + '\n\n' + outcome[0][0] )
-        elif ability_detail == 'X':
-            cursor.execute("SELECT ability_details FROM agents_abilities WHERE ability_name = '" + result[3][0] + "'")
-            outcome = cursor.fetchall()
-            print(result[3][0] + '\n\n' + outcome[0][0] )
-        
-        
+        #Close DB connection
         sqliteConnection.close()
+        #Directs user to role and abilities menu if wanted
         ability_info = input("Would you like to learn about the Agent's role? ").upper()
         if ability_info == 'Y' or ability_info == 'YES':
             get_role_details(role, agent_num)
         else:
+            ##Empty list and end program
             agents_outcome.clear()
-            print('Thank you for using this program!')
+            print(end_program_msg)
     #Handle errors
     except sqlite3.Error as error:
         print('Error occurred - ', error)
--- main/valorant.py
+++ main/valorant.py
@@ -1,5 +1,5 @@
-import random
 import database_connect
+
 #def choose_agent(agent_num):
     #list_of_agents = set(['Brimstone', 'Viper', 'Omen', 'Killjoy', 'Cypher', 'Sova', 'Sage', 'Phoenix',
                       #'Jett', 'Reyna', 'Raze', 'Breach', 'Skye', 'Yoru', 'Astra', 'KAY/O', 'Chamber',
@@ -8,39 +8,76 @@ import database_connect
 
     
     #return print(random.choices(list(list_of_agents), k=agent_num))
+
+
 #Get further information of agents dependin gon user's needs
 def get_agents_info():
     info = input('Would you like some info of the agents?').upper()
     #Check if user requires more info
     if info == 'Y' or info == 'YES':
-        specification = int(input('Please specify which agent by typing the agent number: '))
-        for agent in database_connect.agents_outcome:
-            if specification == agent[0]:
-                wanted_info = int(input('Select one of the option by typing the corrisponding number:\n 1. Role Description\n 2. Agent Abilities\n'))
-                if wanted_info == 1:
-                    database_connect.get_role_details(agent[2], agent[0])
+        #Boolean used to break the nested while loops
+        w_loop = True
+        w_loop2 = True
+        #Start of first while loop
+        while w_loop == True:
+            specification = int(input('Please specify which agent by typing the agent number: '))
+            #For loop to check if the selected agent is in the given list
+            for i,agent in enumerate(database_connect.agents_outcome):
+                #If number present in the least start second while loop
+                if specification == agent[0]:
+                    while w_loop2 == True:
+                        wanted_info = int(input('Select one of the option by typing the corrisponding number:\n 1. Role Description\n 2. Agent Abilities\n'))
+                        #fetch role details data from database_connect.py
+                        if wanted_info == 1:
+                            database_connect.get_role_details(agent[2], agent[0])
+                            #Set boolean to false to break the loops
+                            w_loop = False
+                            w_loop2 = False
+                        #fetch abilities details data from database_connect.py
+                        elif wanted_info == 2:
+                            database_connect.get_ability_details(agent[0], agent[2])
+                            #Set boolean to false to break the loops
+                            w_loop = False
+                            w_loop2 = False
+                        else:
+                            #Warn user and restart loop
+                            print("Please try again and make sure it's the corrisponding info number!")
+
                 else:
-                    database_connect.get_ability_details(agent[0], agent[2])
+                    #Restart loop if inserted number is not in the given list
+                    if i == len(database_connect.agents_outcome) - 1:
+                        print("Please try again and make sure it's the correct agent number!")
+                    
+                    
                     
     else:
+        #Empty list and end program
         database_connect.agents_outcome.clear()
-        print('Thank you for using this program!')
+        print(database_connect.end_program_msg)
 
+#Main function
 def user_input():
-    num = input('How many agents do you need? ')
-    agent_number = int(num)
-    if agent_number > 5:
-        input('Numbers of agents cannot be more than 5, please try again ')
-        return user_input()
-    else:
-        database_connect.database_connection(agent_number)
-        print('Angent n° | Name | Role')
-        for agent in database_connect.agents_outcome:
-            
-            print('-' + ' ' + str(agent[0]) + ' | ' + agent[1] + ' | ' + agent[2])
-        get_agents_info()
-
-
-
+    #Initial message
+    print('Welcome to Agent Selector!\n')
+    #Start of while loop
+    while True:
+        num = input('How many agents do you need? ')
+        agent_number = int(num)
+        #Check if the inputted number is greater than 5
+        if agent_number > 5:
+            #If greater restarts loop
+            print('Numbers of agents cannot be more than 5, please try again!')
+            continue
+        else:
+            #Fetch the needed data from database_connect.py
+            database_connect.database_connection(agent_number)
+            print('Angent n° | Name | Role')
+            #for loop to print the number of agents
+            for agent in database_connect.agents_outcome:
+                print('-' + ' ' + str(agent[0]) + ' | ' + agent[1] + ' | ' + agent[2])
+            #call the function that gives information of each agent
+            get_agents_info()
+            #break loop when done
+            break
 
 user_input()
